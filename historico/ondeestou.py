04bf97e387c2efb27dc3d3d64065a6c4835420f3
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-09-16 17:19:58 -0300

    chore: remove Tkinter app and use CSV
    - Removed frontend.py, backend.py, atelie.db
    - Added historico/main.py (CSV version)
    - Added CSV files for data storage

---
+++ historico/ondeestou.py
@@ -0,0 +1,7 @@
+import os
+
+# Pega o "Current Working Directory" (Diretório de Trabalho Atual)
+pasta_atual = os.getcwd()
+
+print(f"Olá! O Python está executando a partir desta pasta:")
+print(pasta_atual)
